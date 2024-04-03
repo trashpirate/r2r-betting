62e5339a9b5f03e46afd1224840d22086d399975
trashpirate <nadinaoates@gmail.com>
2024-04-03 15:56:58 -0500

    fix navbar and add metadata

--- src/components/buyButton/index.tsx
+++ src/components/buyButton/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function BuyButton({ ca, ticker }: Props) {
     return (
-        <div className="h-10 px-2 py-2 flex items-center gap-2 bg-white/20 backdrop-blur rounded-md border-l-cyan-300 border-l-2 justify-center hover:bg-highlight/50 w-40">
+        <div className="h-10 px-2 py-2 text-sm sm:text-md flex items-center gap-2 bg-white/20 backdrop-blur rounded-md border-l-cyan-300 border-l-2 justify-center hover:bg-highlight/50 w-40 my-auto">
             <Link className="font-bold" href={`https://pancakeswap.finance/swap?chain=bsc&outputCurrency=${ca}`}>{`BUY ${ticker}`}</Link>
         </div>
 
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -15,23 +15,17 @@ type Props = {
 export default function Navbar({ ticker1, ticker2, ca1, ca2 }: Props) {
     return (
         <nav className="my-3 flex justify-between gap-5 align-middle md:w-full">
-            <div className="my-auto h-fit w-fit flex-row rounded-xl border-2 border-white bg-button font-bold text-white hover:bg-buttonHover sm:w-36 sm:justify-between">
-                <a
-                    className="pointer-events-auto mx-auto flex items-center text-right align-middle text-lg uppercase sm:gap-4 lg:p-0"
-                    href="https://buyholdearn.com"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        src="/rugs2riches.jpg"
-                        alt="Rugs2Riches logo"
-                        className="ml-0 h-10 w-auto overflow-hidden rounded-xl p-1"
-                        width={40}
-                        height={40}
-                        priority
-                    />
-                    <div className="w-0 scale-0 sm:w-fit sm:scale-100">Home</div>
-                </a>
-            </div>
+
+            <Link href="https://www.youtube.com/@FromRugs2Riches" className='my-auto h-full'>
+                <Image
+                    src="/rugs2riches.jpg"
+                    alt="Rugs2Riches logo"
+                    className="rounded-lg h-fit w-auto "
+                    width={50}
+                    height={50}
+                    priority
+                />
+            </Link>
             <BuyButton ticker={ticker1} ca={ca1}></BuyButton>
             <BuyButton ticker={ticker2} ca={ca2}></BuyButton>
         </nav>
